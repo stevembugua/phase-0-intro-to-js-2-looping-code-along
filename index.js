3e5b32cc1ab5d2016a34188aea2cfb47f686349e
mbugua <mbuguafaimba@gmail.com>
2022-11-17 18:15:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -8,18 +8,11 @@ function writeCards(array,event){
     return newArray
 }
 
-function printBadges (names) {
-    for (let i = 0; i < names.length; i++) {
-      console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
-    }
-    return names;
-  }
 
 
   function countDown (number){
     
         for(let i = number; !(i<0); i--){
-            let num = number -1
             console.log(i)
         }
 }
